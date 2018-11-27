4ec31126de2603018eb5d8ffd73fa1366255f5f7
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-11-28 02:59:21 +0300

    Lab2 - Done

---
+++ lab2.R
@@ -0,0 +1,45 @@
+gcd <- function(n, m) {
+  if (n == 0) {
+    return(m)
+  } else if (m == 0) {
+    return(n)
+  } else if (n == 1 || m == 1) {
+    return(1)
+  } else if (n == m) {
+    return(n)
+  }
+  
+  nEven = n %% 2 == 0
+  mEven = m %% 2 == 0
+  if (nEven && mEven) {
+    return(2 * gcd(m/2, n/2))
+  } else if (mEven && !nEven) {
+    return(gcd(m/2, n))
+  } else if (!mEven && nEven) {
+    return(gcd(m, n/2))
+  } else if (!mEven && !nEven && n > m) {
+    return(gcd((n-m) / 2, m))
+  } else {
+    return(gcd((m-n) / 2, n))
+  }
+}
+
+gcd(1071, 462) == 21
+
+
+
+gcd_v <- function(...) {
+  args <- c(...)
+  res <- args[1]
+  
+  gcd_internal <- function(x) {  
+    res = gcd(res, x)
+  }
+  
+  #lapply(args, gcd_internal)
+  
+  #return(res)
+  return (lapply(args, sum))
+}
+
+(gcd_v(1, 1071, 462, 1))
