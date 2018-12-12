e2b051f1b9b6187a15971979e27719d31b3f8948
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-12-12 23:14:57 +0300

    Add lab3 task2

--- lab3.R
+++ lab3.R
@@ -37,12 +37,76 @@ shapiro.test(sample.data.frame$X2)
 shapiro.test(sample.data.frame$X3)
 
 #истинные значения
-m1 = Mu[1]
-Sigma12 = Sigma[2, c(2:4)]
-Sigma22 = Sigma[c(2:4), c(2:4)]
+m1 <- Mu[1]
+m2 <- matrix(Mu[2:4], 3, 1)
+Sigma12 <- Sigma[2, c(2:4)]
+Sigma22 <- Sigma[c(2:4), c(2:4)]
+
+sample.coefficients <- Sigma12 %*% solve(Sigma22)
+sample.intercept <- m1 - Sigma12 %*% solve(Sigma22) %*% m2
+
+summary(lm)
+sample.intercept
+sample.coefficients
+
+#Task 2
+#
+#Из файла Lab2Task2Var[16].scv загрузить данные. Данные содержат как значения зависимых переменных, 
+#так и независимых. Вид зависимости известен и задан в таблице. Однако кроме коэффициентов регрессии неизвестен 
+#и коэффициент α. Предложите метод оценивания всех неизвестных коэффициентов с использованием функции lm, и оцените их. 
+#Приведите графическую иллюстрацию полученных результатов (график рассеяния с полученной линией регрессии). Воспользуйтесь
+#функцией nlm, сравните полученные результаты.
+
+sample.df <- read.csv(file="data/Lab3Task2Var16.csv")
+sample.df
+
+# lm
+
+lm.result <- function(alpha, sample) {
+  df <- sample
+  df$x <- sin(alpha * df$x)
+  return( lm(y ~ x, data = df) )
+}
+
+lm.cor <- function(alpha, b0, b1, sample) {
+  test <- b0 + b1*sin(alpha*sample$x)
+  return( cor(test, sample$y) )
+}
 
 
-sample.coefficients = Sigma12 %*% solve(Sigma22)
-sample.intercept = 
+alpha <- 3.5
+step <- 0.1
+max.cor <- 0
+alpha.best <- alpha
+
+for (i in 1:20) {
+  res <- lm.result(alpha, sample.df)  
+  cor <- lm.cor(alpha, res$coefficients[1], res$coefficients[2], sample.df)
+  
+  if (cor > max.cor) {
+    params.best <- res
+    alpha.best <- alpha
+  }
+  
+  alpha <- alpha + step
+}
+
+result.lm <- params.best$coefficients[1] + params.best$coefficients[2]*sin(alpha.best*sample.df$x)
+
+# nlm
+fn <- function(par,  x) {
+  mean((x$y - par[1] - par[2]*sin(par[3]*x$x))^2)
+}
+
+result.nlm.params <- nlm(p = c(03, 0.5, 5), f = fn, x = sample.df)
+result.nlm <- result.nlm.params$estimate[1] + result.nlm.params$estimate[2]*sin(result.nlm.params$estimate[3]*sample.data.frame$x)
+
+plot(y~x, data = sample.df)
+p.order = order(sample.df$x)
+
+lines(sample.df$x[p.order], result.lm[p.order], col = 'red')
+lines(sample.df$x[p.order], result.nlm[p.order], col = 'blue')
+
+result.nlm.params
 
 
